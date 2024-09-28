dd3784b76babac47d54b82c5374a9b96f4ec7d09
oscarthedev15 <jesserjacob99@gmail.com>
2024-09-27 23:59:13 -0400

    links

--- src/app/components/SocialLinksCard.tsx
+++ src/app/components/SocialLinksCard.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { FaDiscord, FaTelegram, FaTwitter } from "react-icons/fa";
+import { FaDiscord, FaTelegram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const socialLinks = [
   {
     name: "Discord",
-    url: "discordapp.com/users/plnkerxd",
-    icon: <FaDiscord />,
-  },
-  { name: "Telegram", url: "https://t.me/camp0_15", icon: <FaTelegram /> },
-  { name: "Twitter", url: "https://twitter.com", icon: <FaTwitter /> },
+    url: "https://discord.com",
+    icon: <FaDiscord className="text-2xl" />,
+  }, // Increase icon size
+  {
+    name: "Telegram",
+    url: "https://t.me/camp0_15",
+    icon: <FaTelegram className="text-2xl" />,
+  }, // Increase icon size
+  {
+    name: "Twitter",
+    url: "https://twitter.com",
+    icon: <FaXTwitter className="text-2xl" />,
+  }, // Increase icon size
 ];
 
 const SocialLinksCard = () => {
   return (
     <motion.div
-      className="flex flex-col items-center p-4 border-2 cursor-pointer font-mplus w-64" // Responsive width and height
+      className="flex flex-col items-center  border-2 cursor-pointer font-mplus w-64" // Responsive width and height
       style={{
         backgroundColor: "#f0f0f0",
         borderColor: "#ccc",
@@ -26,14 +35,17 @@ const SocialLinksCard = () => {
       whileHover={{ scale: 1.05 }} // Add pulsing effect on hover
       transition={{ duration: 0.5 }}
     >
-      <h2 className="text-xl font-bold mb-4 text-center w-full">Contact</h2>
+      <h2 className="text-xl font-bold mb-4 text-center w-full">Contact</h2>{" "}
+      {/* Add Contact title */}
       <div className="flex items-center">
         <img
-          src="/profile-image/profile.svg" // Replace with your image path
+          src="/profile-image/profile.png" // Replace with your image path
           alt="Social Links"
           className="w-16 h-16 md:w-24 md:h-24 object-contain mr-4" // Ensure the image fits within the container
         />
-        <div className="flex flex-col space-y-2">
+        <div className="flex flex-row space-x-4">
+          {" "}
+          {/* Increase spacing between icons */}
           {socialLinks.map((link, index) => (
             <a
               key={index}
@@ -43,7 +55,8 @@ const SocialLinksCard = () => {
               className="flex items-center text-blue-500"
             >
               {link.icon}
-              <span className="ml-2">{link.name}</span>
+              {/* <span className="ml-2 hidden md:inline">{link.name}</span>{" "} */}
+              {/* Hide text on smaller screens */}
             </a>
           ))}
         </div>
